9573e520c9511d3f0b80405764e7b97bcb2eaac6
Ismael Nieto <ismael.nieto@gmail.com>
2021-04-15 11:05:37 +0200

    Rehaciendo el ejercicio

--- optica.js
+++ optica.js
@@ -17,228 +17,202 @@ dbo.createCollection("suppliers", function(err, res) {
   console.log("Collection created!" + res);
 }); 
 
-dbo.createCollection("sales", function(err, res) {
-  if (err) throw err;
-      console.log("Collection created!" + res);
-});
-
-dbo.createCollection("glasses", function(err, res) {
-  if (err) throw err;
-  console.log("Collection created!" + res);
-});
 
 dbo.customers.insertMany(
+  
   [{
-      
-      "lastName": "Garcia",
-      "firstName": "Maria",
-      "address": {
-        "street": "Calle del camino 20",
-        "zipcode": "08025",
-        "city": "Valencia"
-      },
-      "contactDetails": {
-        "email": "eiueiur@gmai.com",
-        "phone": "936528457"
-      },
-      "registeredAT": "2019-09-24",
-      "recommendedBy": "Antonio Perez"
-    },{
-      
-      "lastName": "Perez",
-      "firstName": "Alfonso",
-      "address": {
-        "street": "Calle del camino 20",
-        "zipcode": "08025",
-        "city": "Valencia"
-      },
-      "contactDetails": {
-        "email": "8eiur@hotmail.com",
-        "phone": "909528457"
-      },
-      "registeredAT": "2019-09-24",
-      "recommendedBy": "Sonia Rodriguez"
-    },{
-      
-      "lastName": "martinez",
-      "firstName": "Javier",
-      "address": {
-        "street": "Plaza del pueblo",
-        "zipcode": "08532",
-        "city": "Cadiz"
-      },
-      "contactDetails": {
-        "email": "j.ca@gmai.com",
-        "phone": "9754097"
-      },
-      "registeredAT": "2019-09-24",
-      "recommendedBy": "Antonio Perez"
-    }]
+    "lastName": "Garcia",
+    "firstName": "Maria",
+    "address": {
+      "street": "Calle del camino 20",
+      "zipcode": "08025",
+      "city": "Valencia"
+    },
+    "contactDetails": {
+      "email": "eiueiur@gmai.com",
+      "phone": "936528457"
+    },
+    "registeredAT": "2019-09-24",
+    "recommendedBy": "Antonio Perez",
+    "invoices": [
+      {
+        "id": {
+          "$oid": "60754e4cef864d9e44f7a550"
+        },
+        "items": [
+          {
+            "glassId": 1,
+            "quantity": 2
+          },
+          {
+            "glassID": 2,
+            "quantity": "1"
+          }
+        ],
+        "finalPrice": 200
+      }
+    ]
+  },
+  {
+    "lastName": "Rodriguez",
+    "firstName": "Miguel",
+    "address": {
+      "street": "Plaza santa Ana",
+      "zipcode": "9584",
+      "city": "Madrid"
+    },
+    "contactDetails": {
+      "email": "migel.Rd@yahoo.es",
+      "phone": "600896352"
+    },
+    "registeredAT": "2021-04-14",
+    "recommendedBy": "Antonio Perez",
+    "invoices": [
+      {
+        "id": {
+          "$oid": "60754e4cef864d9e44f7a550"
+        },
+        "items": [
+          {
+            "glassId": 13,
+            "quantity": 1
+          }
+        ],
+        "finalPrice": 80
+      }
+    ]
+  }]
+
 );     
 
 dbo.suppliers.insertMany(
-        [{
-          
-          "name": "MAlly Sourcing LLC",
-          "nif": 938787,
-          "address": {
-            "street": "Subenstrujenbajenstrasse",
-            "number": 750,
-            "floor": 1,
-            "door": null,
-            "zipcode": "22182",
-            "city": "Vienna",
-            "country": "Austria"
-          },
-          "phone": 43589548454,
-          "fax": 43589548455,
-          "brands": [
-            "Ray-Ban",
-            "Dakley",
-            "Polaroid",
-            "Ferrari"
-          ],
-          "orders": [
-            {
-              "orderDate": {
-                "date": "2019-07-10T22:00:00Z"
-              },
-              "orderPrice": 365,
-              "itemsOrdered": [
-                "Impala",
-                "calas",
-                "logi"
-              ],
-              "itemPrice": [
-                50,
-                40,
-                35
-              ],
-              "itemQuantity": [
-                2,
-                4,
-                3
-              ]
-            }
-          ]
-        },{
-          
-          "name": "National Association Glass",
-          "nif": 58998,
-          "address": {
-            "street": "Austin Marketing Str.",
-            "number": 14,
-            "floor": 0,
-            "door": 3,
-            "zipcode": "55908",
-            "city": "London",
-            "country": "United-Kingdom"
-          },
-          "phone": 1207288200,
-          "fax": 1207288203,
-          "brands": [
-            "Dior",
-            "Ferrari",
-            "Boss",
-            "Tous"
-          ]
-        }]
-
-); // insertmany
-    
-
-dbo.glasses.insertMany(
-                    [{
-                      
-                      "name": "Impala",
-                      "price": 90,
-                      "details": {
-                        "diopters": 2,
-                        "color glass": "blue",
-                        "glass frame": "hard",
-                        "color frame": "blue"
-                      },
-                      "brand": {
-                        "oid": "60507e642e7c2f29e619aca1"
-                      }
-                    },{
-                      
-                      "name": "calas",
-                      "price": 120,
-                      "details": {
-                        "diopters": 0.1,
-                        "color glass": "red",
-                        "glass frame": "semi-hard",
-                        "color frame": "blue"
-                      },
-                      "brand": {
-                        "oid": "60507e642e7c2f29e619aca1"
-                      }
-                    },{
-                      
-                      "name": "logi",
-                      "price": 75,
-                      "details": {
-                        "diopters": 0.7,
-                        "color glass": "yellow",
-                        "glass frame": "soft",
-                        "color frame": "white"
-                      },
-                      "brand": {
-                        "oid": "60507e642e7c2f29e619aca2"
-                      }
-                    }]  
-  
-                  
-    
-); // insertmany
-        
-dbo.sales.insertMany(
   [{
-    "_id": {
-      "oid": "60509c192e7c2f29e619acac"
+    "name": "MAlly Sourcing LLC",
+    "nif": 938787,
+    "address": {
+      "street": "Subenstrujenbajenstrasse",
+      "number": 750,
+      "floor": 1,
+      "door": null,
+      "zipcode": "22182",
+      "city": "Vienna",
+      "country": "Austria"
     },
-    "items": [
+    "phone": 43589548454,
+    "fax": 43589548455,
+    "orders": [
       {
-        "glass": {
-          "oid": "60508e712e7c2f29e619aca6"
+        "orderDate": {
+          "$date": "2019-07-10T22:00:00Z"
         },
-        "quantity": 1
+        "orderPrice": 365,
+        "itemsOrdered": [
+          "Impala",
+          "calas",
+          "logi"
+        ],
+        "itemPrice": [
+          50,
+          40,
+          35
+        ],
+        "itemQuantity": [
+          2,
+          4,
+          3
+        ]
+      }
+    ],
+    "glasses": [
+      {
+        "id": "1",
+        "glassName": "Impala",
+        "price": 90,
+        "diopters": "2",
+        "colorGlass": "blue",
+        "frameGlass": "hard",
+        "colorFrame": "blue",
+        "brand": "Ray-Ban"
       },
       {
-        "glass": {
-          "oid": "60508e712e7c2f29e619aca8"
-        },
-        "quantity": 1
-      }
-    ],
-    "finalPrice": 165,
-    "salesDate": "2020-11-01",
-    "salesDoneBy": "Antonio Perez",
-    "customer": {
-      "oid": "605099bf2e7c2f29e619aca9"
-    }
-  },{
-    "_id": {
-      "oid": "60509c192e7c2f29e619acad"
-    },
-    "items": [
+        "id": "2",
+        "glassName": "calas",
+        "price": 40,
+        "diopters": "1",
+        "colorGlass": "black",
+        "frameGlass": "hard",
+        "colorFrame": "blue",
+        "brand": "Ray-Ban"
+      },
       {
-        "glass": {
-          "oid": "60508e712e7c2f29e619aca7"
+        "id": "3",
+        "glassName": "Logi",
+        "price": 90,
+        "diopters": "2.7",
+        "colorGlass": "red",
+        "frameGlass": "semihard",
+        "colorFrame": "red",
+        "brand": "Dakley"
+      }
+    ]
+  },
+  {
+    "name": "Sauge glasses and Sons",
+    "nif": 123455,
+    "address": {
+      "street": "BarracudaStr",
+      "number": 750,
+      "floor": 1,
+      "door": null,
+      "zipcode": "22182",
+      "city": "Londo",
+      "country": "United Kingdom"
+    },
+    "phone": 35689845247,
+    "fax": 35689845289,
+    "orders": [
+      {
+        "orderDate": {
+          "$date": "2021-07-10T22:00:00Z"
         },
-        "quantity": 1
+        "orderPrice": 150,
+        "itemsOrdered": [
+          "Azulonas",
+          "Rojitas"
+        ],
+        "itemPrice": [
+          50,
+          40
+        ],
+        "itemQuantity": [
+          2,
+          2
+        ]
       }
     ],
-    "finalPrice": 120,
-    "salesDate": "2020-05-12",
-    "salesDoneBy": "Antonio Perez",
-    "customer": {
-      "oid": "605099bf2e7c2f29e619acab"
-    }
-  }]                    
-                    
-      
-); // insertmany
+    "glasses": [
+      {
+        "id": "13",
+        "glassName": "Azulonas",
+        "pricePVP": 80,
+        "diopters": 2,
+        "colorGlass": "red",
+        "frameGlass": "soft",
+        "colorFrame": "red",
+        "brand": "MultiOptica"      
+      },
+      {
+       "id": "14",
+        "glassName": "Rojitas",
+        "pricePVP": "56",
+        "diopters": 1.4,
+        "colorGlass": "brown",
+        "frameGlass": "hard",
+        "colorFrame": "black",
+        "brand": "MultiOptica"
+      }
+    ]
+  }]
 
+); // insertmany
         
